a92221de7afe88f2a70ac76cf36cfa97493eb3da
Hubert Kieruczenko <Hub3rt65@gmail.com>
2025-10-17 08:44:32 +0200

    import loginData for login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,16 @@
 import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
 
-test.describe('user login to demobank', () => {
+
+test.describe('user login to demobank', () => {;
   test('successful login with valid credentials', async ({ page }) => {
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
     await page.goto('https://demo-bank.vercel.app/');
     await page.getByTestId('login-input').click();
-    await page.getByTestId('login-input').fill('testerhk');
+    await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').click();
-    await page.getByTestId('password-input').fill('Password');
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
   });
 });
