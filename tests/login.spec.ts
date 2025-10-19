f8ac0629391f399e697e1fe2e3e1097d26580334
Hubert Kieruczenko <Hub3rt65@gmail.com>
2025-10-19 15:28:56 +0200

    add unsuccessful login test for short username in login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data';
 
-
 test.describe('user login to demobank', () => {
   test('successful login with valid credentials', async ({ page }) => {
     const userId = loginData.userId;
@@ -12,5 +11,17 @@ test.describe('user login to demobank', () => {
     await page.getByTestId('password-input').click();
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    const invalidUserId = 'hk';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+    await page.goto('/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill(invalidUserId);
+    await page.getByTestId('password-input').click();
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorMessage
+    );
   });
 });
