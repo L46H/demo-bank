ef682b3eba30d9005b2c4167214b6a846f4c7c82
Hubert Kieruczenko <Hub3rt65@gmail.com>
2025-10-21 07:57:44 +0200

    refactor page navigation to beforeEach in login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,10 +2,13 @@ import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data';
 
 test.describe('user login to demobank', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
   test('successful login with valid credentials', async ({ page }) => {
     const userId = loginData.userId;
     const userPassword = loginData.userPassword;
-    await page.goto('/');
     await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').click();
@@ -16,7 +19,6 @@ test.describe('user login to demobank', () => {
   test('unsuccessful login with too short username', async ({ page }) => {
     const invalidUserId = 'hk';
     const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
-    await page.goto('/');
     await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill(invalidUserId);
     await page.getByTestId('password-input').click();
@@ -28,7 +30,6 @@ test.describe('user login to demobank', () => {
   test('unsuccessful login with too short password', async ({ page }) => {
     const invalidPassword = 'hk';
     const expectedErrorMessage = 'hasło ma min. 8 znaków';
-    await page.goto('/');
     await page.getByTestId('password-input').fill(invalidPassword);
     await page.getByTestId('password-input').blur();
     await expect(page.getByTestId('error-login-password')).toHaveText(
