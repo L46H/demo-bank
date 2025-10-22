bca81de352c38204acd29b05546ee46aa5ae7bc0
Hubert Kieruczenko <Hub3rt65@gmail.com>
2025-10-22 08:25:03 +0200

    refactor use LoginPage class in login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,26 +1,25 @@
 import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
 
 test.describe('user login to demobank', () => {
+  let loginPage: LoginPage;
+
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
+    loginPage = new LoginPage(page);
   });
 
   test('successful login with valid credentials', async ({ page }) => {
     const userId = loginData.userId;
     const userPassword = loginData.userPassword;
-    await page.getByTestId('login-input').click();
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').click();
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
+    await loginPage.login(userId, userPassword);
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
     const invalidUserId = 'hk';
     const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
-    await page.getByTestId('login-input').click();
-    await page.getByTestId('login-input').fill(invalidUserId);
+    await loginPage.loginInput.fill(invalidUserId);
     await page.getByTestId('password-input').click();
     await expect(page.getByTestId('error-login-id')).toHaveText(
       expectedErrorMessage
@@ -30,7 +29,7 @@ test.describe('user login to demobank', () => {
   test('unsuccessful login with too short password', async ({ page }) => {
     const invalidPassword = 'hk';
     const expectedErrorMessage = 'hasło ma min. 8 znaków';
-    await page.getByTestId('password-input').fill(invalidPassword);
+    await loginPage.passwordInput.fill(invalidPassword);
     await page.getByTestId('password-input').blur();
     await expect(page.getByTestId('error-login-password')).toHaveText(
       expectedErrorMessage
