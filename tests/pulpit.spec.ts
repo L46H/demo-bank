03da67c4a0cf3e68880bc7deec08e82d3bef46b0
Hubert Kieruczenko <Hub3rt65@gmail.com>
2025-11-12 21:49:46 +0100

    add successful mobile top-up test in pulpit.spec.ts

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -24,6 +24,27 @@ test.describe('pulpit tests', () => {
     await page.getByRole('button', { name: 'wykonaj' }).click();
     await page.getByTestId('close-button').click();
 
-    await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Jan Demobankowy - 100,00PLN - uber');
+    await expect(page.locator('#show_messages')).toHaveText(
+      'Przelew wykonany! Jan Demobankowy - 100,00PLN - uber'
+    );
+  });
+
+  test('successful mobile top-up', async ({ page }) => {
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerhk');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('Password123*');
+    await page.getByTestId('login-button').click();
+
+    await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+    await page.locator('#widget_1_topup_amount').click();
+    await page.locator('#widget_1_topup_amount').fill('50');
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    await expect(page.locator('#show_messages')).toHaveText(
+      'Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx'
+    );
   });
 });
